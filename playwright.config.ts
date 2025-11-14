7de9a0a040c0ee0abcc95c2738cfe4f041432464
aidandf29 <muhammad.aidan@ui.ac.id>
2025-11-14 12:21:42 +0700

    learning with jenkins

--- playwright.config.ts
+++ playwright.config.ts
@@ -92,10 +92,10 @@ export default defineConfig({
       },
     },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
     // {
     //   name: "webkit",
