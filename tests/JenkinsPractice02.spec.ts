64f98a03ee573cff045d564862059520d2ba476b
aidandf29 <muhammad.aidan@ui.ac.id>
2025-11-14 10:43:24 +0700

    learning with jenkins

--- tests/JenkinsPractice02.spec.ts
+++ tests/JenkinsPractice02.spec.ts
@@ -12,7 +12,7 @@ test("Practice Test 5", async ({ page }) => {
   console.log("Starting Practice Test 5");
   await page.goto("https://www.google.com/");
   console.log(await page.title());
-  await expect(page).toHaveTitle("GoogleWrong");
+  await expect(page).toHaveTitle("GoogleWronggggg");
   console.log("Ending Practice Test 5");
 });
 
